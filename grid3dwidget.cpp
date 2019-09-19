d9d51426b2f2ab3ee0d515e39389660aaf48dbf1
Whu-wxy <641645560@qq.com>
2019-09-19 17:58:41 +0800

    fix bug and new version

--- grid3dwidget.cpp
+++ grid3dwidget.cpp
@@ -4,8 +4,24 @@
 Grid3DWidget::Grid3DWidget(QWidget *parent)
     : QWidget(parent)
 {
-    h = 200;
-    rotateX = -30;
+//    h = 200;
+//    rotateX = -30;
+//    rotateY = 0;
+//    rotateZ = 0;
+
+//    verticalAngle = 90;
+//    aspectRatio = 16/9.0;
+//    nearPlane = 0.1;
+//    farPlane = 1000;
+
+    setMouseTracking(true);
+
+    gridRatio = 2;
+
+    step = 0.1;
+
+    h = 1.225;
+    rotateX = 45;
     rotateY = 0;
     rotateZ = 0;
 
@@ -14,7 +30,7 @@ Grid3DWidget::Grid3DWidget(QWidget *parent)
     nearPlane = 0.1;
     farPlane = 1000;
 
-    update();
+
 }
 
 Grid3DWidget::~Grid3DWidget()
@@ -31,13 +47,179 @@ void Grid3DWidget::paintEvent(QPaintEvent*)
     painter.drawLine(QPoint(0, this->height()/2), QPoint(this->width(), this->height()/2));
     painter.drawLine(QPoint(this->width()/2, 0), QPoint(this->width()/2, this->height()));
 
+    painter.drawEllipse(ptScene2Screen(QPointF(0.1, 1)), 5, 5);
+    painter.drawEllipse(ptScene2Screen(QPointF(-0.3, 1)), 5, 5);
+
     paintGrid(&painter);
 }
 
 void Grid3DWidget::paintGrid(QPainter* painter)
 {
-    float step = 40;
-    float maxDis = h/tan(-rotateX*M_PI/180);
+    //单位米
+    float maxZVal = maxDis/cos(rotateX*M_PI/180);
+    float minZVal = maxZVal-maxDis*cos(rotateX*M_PI/180);
+    painter->drawText(QPoint(0, this->height()/2), QString::number(maxDis));
+    painter->drawText(QPoint(0, this->height()), QString::number(minDis));
+
+    painter->translate(this->width()/2-ptMiddle.x(), this->height()/2-ptMiddle.y());
+    for(float i=maxDis; i>=0; i-=step)
+    {
+        float zVal = maxZVal-(maxDis-i)*cos(rotateX*M_PI/180);
+
+        QVector3D pH1(-gridRatio*(maxDis-0)/2, i, zVal);
+        QPointF ph1 = matrix.map(pH1).toPointF();
+        QVector3D pH2(gridRatio*(maxDis-0)/2, i, zVal);
+        QPointF ph2 = matrix.map(pH2).toPointF();
+
+        // H line
+        painter->drawLine(QPoint(ptMiddle.x()+(ph1.x()-ptMiddle.x())*scaleFactor, ptMiddle.y()+(ph1.y()-ptMiddle.y())*scaleFactor),
+                          QPoint(ptMiddle.x()+(ph2.x()-ptMiddle.x())*scaleFactor, ptMiddle.y()+(ph2.y()-ptMiddle.y())*scaleFactor));
+    }
+    for(float i=0; i<=maxDis; i+=step)
+    {
+        //左竖线
+        QVector3D pV1(gridRatio*i/2, maxDis, maxZVal);
+        QPointF pv1 = matrix.map(pV1).toPointF();
+        QVector3D pV2(gridRatio*i/2, 0, minZVal);
+        QPointF pv2 = matrix.map(pV2).toPointF();
+
+        //右竖线
+        QVector3D pV3(-gridRatio*i/2, maxDis, maxZVal);
+        QPointF pv3 = matrix.map(pV3).toPointF();
+        QVector3D pV4(-gridRatio*i/2, 0, minZVal);
+        QPointF pv4 = matrix.map(pV4).toPointF();
+
+        // V line
+        painter->drawLine(QPoint(ptMiddle.x()+(pv1.x()-ptMiddle.x())*scaleFactor, ptMiddle.y()+(pv1.y()-ptMiddle.y())*scaleFactor),
+                          QPoint(ptMiddle.x()+(pv2.x()-ptMiddle.x())*scaleFactor, ptMiddle.y()+(pv2.y()-ptMiddle.y())*scaleFactor));
+        painter->drawLine(QPoint(ptMiddle.x()+(pv3.x()-ptMiddle.x())*scaleFactor, ptMiddle.y()+(pv3.y()-ptMiddle.y())*scaleFactor),
+                          QPoint(ptMiddle.x()+(pv4.x()-ptMiddle.x())*scaleFactor, ptMiddle.y()+(pv4.y()-ptMiddle.y())*scaleFactor));
+    }
+
+}
+
+bool Grid3DWidget::papareData()
+{
+    if(h <= 0 || rotateX <= 0)
+        return false;
+
+    //单位米
+    maxDis = h/tan(rotateX*M_PI/180);
+    float maxZVal = maxDis/cos(rotateX*M_PI/180);
+
+    //变换矩阵
+    matrix.translate(this->width()/2, this->height(), 0);
+    matrix.rotate(-rotateX, 1, 0, 0);
+    matrix.rotate(rotateY, 0, 1, 0);
+    matrix.rotate(rotateZ, 0, 0, 1);
+    matrix.translate(0, 0, -h);
+    matrix.perspective(verticalAngle,aspectRatio,nearPlane,farPlane);
+
+
+    QVector3D pFar(0, maxDis, maxZVal);
+    ptMiddle = matrix.map(pFar).toPointF();
+    float near2FarDis = maxZVal * sin(verticalAngle*M_PI/180/2) / sin((rotateX+verticalAngle/2)*M_PI/180);//地面上中心点与最近点米数
+    minDis = maxDis - near2FarDis;
+
+    QVector3D pNear(0, maxDis-near2FarDis, maxZVal-near2FarDis*cos(rotateX*M_PI/180));
+    QPointF pN = matrix.map(pNear).toPointF();
+    scaleFactor = this->height()/2 / fabs(ptMiddle.y()-pN.y());//米->像素
+
+
+    QVector3D pNormal(maxDis, maxDis, maxZVal);
+    pNormal = matrix.map(pNormal);
+
+    pFar = matrix.map(pFar);
+    pNear = matrix.map(pNear);
+
+    //平面方程
+    float x1 = pNormal.x();
+    float y1 = pNormal.y();
+    float z1 = pNormal.z();
+    float x2 = pFar.x();
+    float y2 = pFar.y();
+    float z2 = pFar.z();
+    float x3 = pNear.x();
+    float y3 = pNear.y();
+    float z3 = pNear.z();
+
+
+    A = y1*(z2-z3) + y2*(z3-z1) + y3*(z1-z2);
+    B = z1*(x2-x3) + z2*(x3-x1) + z3*(x1-x2);
+    C = x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2);
+    D = -x1*(y2*z3-y3*z2) - x2*(y3*z1-y1*z3) - x3*(y1*z2-y2*z1);
+
+    return true;
+}
+
+QPoint Grid3DWidget::ptScene2Screen(QPointF scenePoint)
+{
+    //单位米
+    if(scenePoint.y() > maxDis || scenePoint.y() < 0)
+        return QPoint(0, 0);
+
+    float theltaX = this->width()/2-ptMiddle.x();    //与屏幕中心对齐
+    float theltaY = this->height()/2-ptMiddle.y();
+
+    float zVal = maxDis/cos(rotateX*M_PI/180)-(maxDis-scenePoint.y())*cos(rotateX*M_PI/180);
+
+    QVector3D scenePt3D(-scenePoint.x(), scenePoint.y(), zVal);
+    QPointF screenPt = matrix.map(scenePt3D).toPointF();
+    screenPt = QPoint(ptMiddle.x()+(screenPt.x()-ptMiddle.x())*scaleFactor, ptMiddle.y()+(screenPt.y()-ptMiddle.y())*scaleFactor);
+    screenPt.setX(screenPt.x() + theltaX);
+    screenPt.setY(screenPt.y() + theltaY);
+
+    return screenPt.toPoint();
+}
+
+QPointF Grid3DWidget::ptScreen2Scene(QPoint screenPt)
+{
+    if(screenPt.y() < this->height()/2)
+        return QPoint(0, 0);
+
+    float theltaX = this->width()/2-ptMiddle.x();    //与屏幕中心对齐
+    float theltaY = this->height()/2-ptMiddle.y();
+    screenPt.setX(screenPt.x() - theltaX);
+    screenPt.setY(screenPt.y() - theltaY);
+
+    QPointF newScreenPt;
+    newScreenPt.setX(ptMiddle.x()+(screenPt.x()-ptMiddle.x())/scaleFactor);
+    newScreenPt.setY(ptMiddle.y()+(screenPt.y()-ptMiddle.y())/scaleFactor);
+
+    float zVal = -(D + A*newScreenPt.x() + B*newScreenPt.y()) / C;
+    QVector3D scenePt3D(newScreenPt.x(), newScreenPt.y(), zVal);
+    bool invertable = true;
+    if(matrix.determinant() == 0)
+        invertable = false;
+    QMatrix4x4 invertedMatrix = matrix.inverted(&invertable);
+    QVector3D originScenePt3D = invertedMatrix.map(scenePt3D);
+    return QPointF(originScenePt3D.x(), originScenePt3D.y());
+}
+
+void Grid3DWidget::showEvent(QShowEvent* event)
+{
+    papareData();
+    update();
+}
+
+void Grid3DWidget::mouseMoveEvent(QMouseEvent* event)
+{
+    QPoint  mousePt = event->pos();
+    QPoint globalPt = event->globalPos();
+
+    if(mousePt.y() < this->height()/2)
+        return;
+
+    scenePt = ptScreen2Scene(mousePt);
+    QString ptTip = QString("(%1,%2)").arg(-scenePt.x()).arg(scenePt.y());
+    QToolTip::showText(globalPt, ptTip, this, this->rect());
+
+    update();
+}
+
+void Grid3DWidget::originPaintGrid(QPainter* painter)
+{
+    //单位米
     painter->drawText(QPoint(0, this->height()/2), QString::number(maxDis));
     float minDis = 0;
 
@@ -47,55 +229,55 @@ void Grid3DWidget::paintGrid(QPainter* painter)
     QMatrix4x4 mat;
 
     mat.translate(this->width()/2, this->height(), 0);
-    mat.rotate(rotateX, 1, 0, 0);
+    mat.rotate(-rotateX, 1, 0, 0);
     mat.rotate(rotateY, 0, 1, 0);
     mat.rotate(rotateZ, 0, 0, 1);
-    mat.translate(0,0, -h);
+    mat.translate(0, 0, -h);
     mat.perspective(verticalAngle,aspectRatio,nearPlane,farPlane);
 
 
     QVector3D pFar(0, maxDis, maxZVal);
-    QPointF pF = mat.map(pFar).toPointF();
-    float near2FarDis = maxDis * sin(verticalAngle*M_PI/180/2) / sin((-rotateX+verticalAngle/2)*M_PI/180);//地面上中心点与最近点米数
-    painter->drawText(QPoint(0, this->height()), QString::number(maxDis-near2FarDis));
+    QPointF ptMiddle = mat.map(pFar).toPointF();
+    float near2FarDis = maxZVal * sin((verticalAngle/2)*M_PI/180) / sin((rotateX+verticalAngle/2)*M_PI/180);//地面上中心点与最近点米数
+    painter->drawText(QPoint(0, this->height()), QString::number(minDis));
 
     QVector3D pNear(0, maxDis-near2FarDis, maxZVal-near2FarDis*cos(rotateX*M_PI/180));
     QPointF pN = mat.map(pNear).toPointF();
-    double factor = this->height()/2 / fabs(pF.y()-pN.y());
+    double factor = this->height()/2 / fabs(ptMiddle.y()-pN.y());
+    //单位米->像素数
 
-    painter->translate(this->width()/2-pF.x(), this->height()/2-pF.y());
-    for(int i=maxDis; i>=minDis; i-=step)
+    painter->translate(this->width()/2-ptMiddle.x(), this->height()/2-ptMiddle.y());
+    for(float i=maxDis; i>=minDis; i-=step)
     {
         float zVal = maxZVal-(maxDis-i)*cos(rotateX*M_PI/180);
 
-        QVector3D pH1(-(maxDis-minDis)/2, i, zVal);
+        QVector3D pH1(-gridRatio*(maxDis-minDis)/2, i, zVal);
         QPointF ph1 = mat.map(pH1).toPointF();
-        QVector3D pH2((maxDis-minDis)/2, i, zVal);
+        QVector3D pH2(gridRatio*(maxDis-minDis)/2, i, zVal);
         QPointF ph2 = mat.map(pH2).toPointF();
 
         // H line
-        painter->drawLine(QPoint(pF.x()+(ph1.x()-pF.x())*factor, pF.y()+(ph1.y()-pF.y())*factor),
-                          QPoint(pF.x()+(ph2.x()-pF.x())*factor, pF.y()+(ph2.y()-pF.y())*factor));
+        painter->drawLine(QPoint(ptMiddle.x()+(ph1.x()-ptMiddle.x())*factor, ptMiddle.y()+(ph1.y()-ptMiddle.y())*factor),
+                          QPoint(ptMiddle.x()+(ph2.x()-ptMiddle.x())*factor, ptMiddle.y()+(ph2.y()-ptMiddle.y())*factor));
     }
-    for(int i=minDis; i<=maxDis; i+=step)
+    for(float i=minDis; i<=maxDis; i+=step)
     {
-        //右竖线
-        QVector3D pV1((i-minDis)/2, maxDis, maxZVal);
+        //左竖线
+        QVector3D pV1(gridRatio*(i-minDis)/2, maxDis, maxZVal);
         QPointF pv1 = mat.map(pV1).toPointF();
-        QVector3D pV2((i-minDis)/2, minDis, minZVal);
+        QVector3D pV2(gridRatio*(i-minDis)/2, minDis, minZVal);
         QPointF pv2 = mat.map(pV2).toPointF();
 
-        //左竖线
-        QVector3D pV3(-(i-minDis)/2, maxDis, maxZVal);
+        //右竖线
+        QVector3D pV3(-gridRatio*(i-minDis)/2, maxDis, maxZVal);
         QPointF pv3 = mat.map(pV3).toPointF();
-        QVector3D pV4(-(i-minDis)/2, minDis, minZVal);
+        QVector3D pV4(-gridRatio*(i-minDis)/2, minDis, minZVal);
         QPointF pv4 = mat.map(pV4).toPointF();
 
         // V line
-        painter->drawLine(QPoint(pF.x()+(pv1.x()-pF.x())*factor, pF.y()+(pv1.y()-pF.y())*factor),
-                          QPoint(pF.x()+(pv2.x()-pF.x())*factor, pF.y()+(pv2.y()-pF.y())*factor));
-        painter->drawLine(QPoint(pF.x()+(pv3.x()-pF.x())*factor, pF.y()+(pv3.y()-pF.y())*factor),
-                          QPoint(pF.x()+(pv4.x()-pF.x())*factor, pF.y()+(pv4.y()-pF.y())*factor));
+        painter->drawLine(QPoint(ptMiddle.x()+(pv1.x()-ptMiddle.x())*factor, ptMiddle.y()+(pv1.y()-ptMiddle.y())*factor),
+                          QPoint(ptMiddle.x()+(pv2.x()-ptMiddle.x())*factor, ptMiddle.y()+(pv2.y()-ptMiddle.y())*factor));
+        painter->drawLine(QPoint(ptMiddle.x()+(pv3.x()-ptMiddle.x())*factor, ptMiddle.y()+(pv3.y()-ptMiddle.y())*factor),
+                          QPoint(ptMiddle.x()+(pv4.x()-ptMiddle.x())*factor, ptMiddle.y()+(pv4.y()-ptMiddle.y())*factor));
     }
-
 }
--- grid3dwidget.h
+++ grid3dwidget.h
@@ -7,6 +7,8 @@
 #include <QMatrix4x4>
 #include <QPaintEvent>
 #include <QPainter>
+#include <QMouseEvent>
+#include <QToolTip>
 
 class Grid3DWidget : public QWidget
 {
@@ -16,18 +18,43 @@ public:
     Grid3DWidget(QWidget *parent = 0);
     ~Grid3DWidget();
 
+    QMatrix4x4 matrix;
+    double  scaleFactor;        //米->像素
+    QPointF ptMiddle;           //屏幕中心点，视线中心线上的点
+
+    float   maxDis;             //米
+    float   minDis;
+    float   gridRatio;          //场景宽高比
+
+    //
     float   rotateX;
     float   rotateY;
     float   rotateZ;
     float   h;
+    float   step;
 
     float   verticalAngle;
     float   aspectRatio;
     float   nearPlane;
     float   farPlane;
 
-    void    paintEvent(QPaintEvent*);
+    float   A;
+    float   B;
+    float   C;
+    float   D;
+
+    QPointF scenePt;
+
+    bool    papareData();
+    QPoint  ptScene2Screen(QPointF);
+    QPointF ptScreen2Scene(QPoint);
+
+    virtual void    showEvent(QShowEvent*);
+    virtual void    mouseMoveEvent(QMouseEvent*);
+    virtual void    paintEvent(QPaintEvent*);
     void    paintGrid(QPainter*);
+
+    void    originPaintGrid(QPainter*);  //仅留作纪念
 };
 
 #endif // GRID3DWIDGET_H
